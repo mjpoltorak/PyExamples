379fa30bd85a986d77c4b85c466d2f78b97124cd
Matthew Poltorak <mpoltorak@stansberryresearch.com>
2017-05-24 14:37:12 -0400

    Working commit

--- Bloomberg Data Analysis v1.py
+++ Bloomberg Data Analysis v1.py
@@ -10,14 +10,14 @@ import matplotlib.pyplot as plt
 import pandas as pd #this is how I usually import pandas
 import sys #only needed to determine Python version number
 import matplotlib #only needed to determine Matplotlib version number
-import numpy
+import math
 
 print('Python version ' + sys.version)
 print('Pandas version ' + pd.__version__)
 print('Matplotlib version ' + matplotlib.__version__)
 
 
-Location = 'C:\Users\BLPAPI_DATA.csv'
+Location = "C:/Users/mpoltorak/Developement/workspace/PyExamples/dataFrameExample/BLPAPI_Data.csv"
 
 '''
 df = pd.io.parsers.read_csv(Location, header=[0,1], parse_dates=[['Security'],['Field']])
@@ -30,22 +30,23 @@ print df[0:5]
 df = pd.read_csv(Location, parse_dates=['Security'])
 #df.rename(columns = {'Security':'Date'}, inplace=True)
 #df.interpolate(method='linear', axis=0, inplace=True)
-print df[0:30]
+print (df[0:30])
+print("")
 
 
 
-print df.dtypes
+print (df.dtypes)
 
 
-mavg = df['10_MA'] = pd.rolling_mean(df['NDDUUS Index'], 10, min_periods=10)
+mavg = df['10_MA'] = df['MXUS Index'].rolling(window=10, min_periods=10, center=False).mean()
 df['10_MA'].tail()
-print df[0:30]
+print (df[0:60])
 
-mavg = df['30_MA'] = pd.rolling_mean(df['NDDUUS Index'], 20, min_periods=20)
+mavg = df['30_MA'] = df['MXUS Index'].rolling(window=20,min_periods=20,center=False).mean()
 df['30_MA'].tail()
-print df[0:30]
+print (df[0:30])
 
-checkma = df['NDDUUS Data Check'] = numpy.isnan(df['NDDUUS Index'])
-df['NDDUUS Data Check']
+# checkma = df['NDDUUS Data Check'] = math.isnan(df['NDDUUS Index'])
+# df['NDDUUS Data Check']
 
-print df[0:30]
+print (df[0:30])
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # PyExamples
+To run, change the path of BLPAP_Data.csv to a valid path to this document (which is available in the repository). Please ensure you have all necessary libraries installed before running. This is an example of how to import data from a .csv file into a data frame and then append the data frame.
