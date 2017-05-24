d60ea74529e08363417bea4d4ae76a538310b164
Matthew Poltorak <mpoltorak@stansberryresearch.com>
2017-05-24 15:23:17 -0400

    Update all

--- Bloomberg Data Analysis v1.py
+++ Bloomberg Data Analysis v1.py
@@ -1,23 +1,16 @@
-# Import all libraries needed for the tutorial
-
-# General syntax to import specific functions in a library: 
+# General syntax to import specific functions in a library:
 ##from (library) import (specific library function)
-from pandas import DataFrame, read_csv
 
-# General syntax to import a library but no functions: 
+import sys  # only needed to determine Python version number
+
+# General syntax to import a library but no functions:
 ##import (library) as (give the library a nickname/alias)
-import matplotlib.pyplot as plt
-import pandas as pd #this is how I usually import pandas
-import sys #only needed to determine Python version number
-import matplotlib #only needed to determine Matplotlib version number
-import math
+import pandas as pd  # this is how I usually import pandas
 
 print('Python version ' + sys.version)
 print('Pandas version ' + pd.__version__)
-print('Matplotlib version ' + matplotlib.__version__)
 
-
-Location = "C:/Users/mpoltorak/Developement/workspace/PyExamples/dataFrameExample/BLPAPI_Data.csv"
+Location = "./BLPAPI_Data.csv"
 
 '''
 df = pd.io.parsers.read_csv(Location, header=[0,1], parse_dates=[['Security'],['Field']])
